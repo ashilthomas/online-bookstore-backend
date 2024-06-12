cf8a874548cfb7c58b5ea460b2c243aae1fcbb75
Ashil Thomas <ashilthomas31@gmail.com>
2024-06-12 19:53:35 +0530

    origin error

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({ 
-    origin: "https://online-bookstore-web.netlify.app/",
+    origin: "https://online-bookstore-web.netlify.app",
     credentials: true
    
   })
